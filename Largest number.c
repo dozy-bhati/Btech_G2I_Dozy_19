9dcd77c1db2de0e582216ca7ac0bdf461b94d2a3
dozy-bhati <25121cn161@glbitm.ac.in>
2026-02-12 11:42:36 +0530

    Add files via upload

---
+++ Largest number.c
@@ -0,0 +1,20 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+int main() 
+{
+    int a,b,c;
+    printf("Enter the numbers:\n");
+    scanf("%d%d%d", &a,&b,&c);
+    if(a>b && a>c)
+     { printf("a is largest");
+     }
+    else if (b>a && b>c)
+     { 
+         printf("b is largest");
+     }
+    else
+    {
+    printf("c is largest");
+    }
+    return 0;
+}
