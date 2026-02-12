1c44689d72fcde7377dc2194750dab557d9610e8
dozy-bhati <25121cn161@glbitm.ac.in>
2026-02-12 11:36:03 +0530

    Add files via upload

---
+++ Even or Odd.c
@@ -0,0 +1,16 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+int main() 
+{
+    int a,b;
+    printf("Enter the number:\n");
+    scanf("%d", &a);
+    if(a%2==0)
+    {
+        printf("Number is EVEN");
+    }
+    else
+    { printf("Numbers is ODD");
+    }
+    return 0;
+}
