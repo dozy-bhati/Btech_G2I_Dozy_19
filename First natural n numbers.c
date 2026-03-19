a4537db1459a96ed952053e4b2290fc689299cb8
dozy-bhati <25121cn161@glbitm.ac.in>
2026-03-19 10:03:37 +0530

    Add files via upload

---
+++ First natural n numbers.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    // Write C code here
+    int m;
+    printf("Enter m:");
+    scanf("%d",&m);
+    for(int i=1; i<=m; i++)
+    printf("%d",i);
+    return 0;
+}
