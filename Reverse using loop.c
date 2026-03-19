4e85b49efcc6a12672d9683b5b354e7afcf7e985
dozy-bhati <25121cn161@glbitm.ac.in>
2026-03-19 10:10:09 +0530

    Add files via upload

---
+++ Reverse using loop.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    // Write C code here
+    int n,rev=0;
+    printf("Enter a number:");
+    scanf("%d",&n);
+    while(n!=0) {
+        rev=rev*10+ n%10;
+        n/=10;
+    }
+    printf("Reversed = %d", rev);
+    return 0;
+}
